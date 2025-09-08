411881ffed61a19152fecbd6ef1f50b3db3c1786
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-09-08 02:59:47 -0700

    Revert "increment decrement logics added, with the concept data binding applied"
    This reverts commit b6d5650330743e0b8883a31e567bb35ecca97835.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,9 @@
-import React, { use, useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Card = ({ products, cartCount, setCartCount, type}) => {
-const [itemCount,setCount] = useState(0);
-const itemIncrement = () =>{
-    type == 'cart' ? setCartCount(prev => prev + 1) : setCount(prev => prev + 1);
-}
-const itemDecrement = () =>{
-        type == 'cart' ? setCartCount(prev => prev > 0 ? prev - 1 : 0) : setCount(prev => prev > 0 ? prev - 1 : 0 );
-}
-// if(type === 'cart'){
-// setCount(products[0]?.itemCount || 0)
-// }
-// console.log(type,"type")
-    const navigate = useNavigate();
+const Card = ({ products }) => {
+
+const navigate = useNavigate();
     return (
         <>
             {
@@ -30,15 +20,7 @@ const itemDecrement = () =>{
                                     <h4>{item.rating.count}</h4>
                                 </div>
                                 <p>{item.description}</p>
-
-                                <div className='d-flex justify-content-between align-items-center'>
-                                    <button onClick={() => navigate('/addToCart', { state: { product: item, itemCount } })} className=''>Add To Cart</button>
-<div className='d-flex justify-content-around align-items-center'>
-    <button onClick={()=>{itemIncrement()}}>+</button>
-  {type == "cart" ? <div className="mx-2" >{cartCount}</div> : <div className="mx-2" >{itemCount}</div>}  
-    <button onClick={()=>{itemDecrement()}}>-</button>
-</div>
-                                </div>
+                                <button onClick={()=>navigate('/addToCart',{state: {product:item}})} className=''>Add To Cart</button>
                             </div>
                         </div>
                     )
--- src/pages/AddToCart.jsx
+++ src/pages/AddToCart.jsx
@@ -5,27 +5,19 @@ import Card from '../components/Card/Card';
 const AddToCart = () => {
 
   const location = useLocation();
-  console.log(location?.state?.product,"location")
+  console.log(location.state.product,"location")
 
   const [products, setProducts] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
    useEffect(() => {
-    if (location?.state?.product) {
-      setProducts([location?.state?.product]);
-    }
-  }, [location.state]);
-   useEffect(() => {
-    if (location?.state?.itemCount) {
-      setCartCount([location?.state?.itemCount]);
+    if (location.state?.product) {
+      setProducts([location.state.product]);
     }
   }, [location.state]);
   console.log(products,"working")
   return (
 <>
     <div>AddToCart</div>
- {
-  products.length > 0 ? <Card products={products} cartCount={cartCount} setCartCount={setCartCount} type={"cart"} /> : <p>No products in cart</p>
- }
+<Card products={products} />
 </>
   )
 }
